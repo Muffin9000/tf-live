01dfd5315cf3637be68ba78a820227629625902b
Konstantins Mjazins <kmjazins@onairent.live>
2022-10-26 19:25:34 +0300

    remplace aws_instance with aws_launch_configuration with autoscaling group

--- main.tf
+++ main.tf
@@ -2,13 +2,56 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_launch_configuration" "example" {
   
-  ami = "ami-06640050dc3f556bb"
+  image_id = "ami-06640050dc3f556bb"
   instance_type = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
 
-  tags = {
-    Name = "terraform-example"
+  user_data = <<-EOF
+    #!/bin/bash
+
+    echo "Hello" > index.html
+    nohup busybox httpd -f -p ${var.port} &
+    EOF
+
+  lifecycle {
+    create_before_destroy = true
   }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.port
+    to_port     = var.port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "port" {
+    description = "set igress from and to port for the security policy"
+    type = number
+    default = 8080
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
 
